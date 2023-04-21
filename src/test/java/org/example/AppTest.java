7073ccfa2138010809a89dc5ec6020bf3b6822dd
jdev08415@gmail.com <jdev08415@gmamil.com>
2023-04-21 10:43:02 +0530

    first commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.ResourceBundle;
+
+
+public class AppTest
+{
+    WebDriver driver;
+    @BeforeClass
+    @Parameters({"browser","url"})
+    void setupdrive(String browser,String link)
+    {
+        if(browser.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            driver=new ChromeDriver();
+        }
+        else if(browser.equalsIgnoreCase("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            driver=new FirefoxDriver();
+        }
+        driver.get(link);
+        driver.manage().window().maximize();
+    }
+
+
+
+    @Test
+    void dropdownmethod()throws InterruptedException{
+
+        Thread.sleep(8000);
+        driver.findElement(By.xpath("/html/body/div/div[2]/div/div/select[1]/option[3]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div/div[2]/div/div/select[2]/option[3]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div/div[2]/div/div/select[3]/option[3]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div/div[3]/div/div/label[2]/input")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div/div[4]/div/div/form/input[3]")).click();
+        Thread.sleep(3000);
+
+    }
+
+
+    @AfterClass
+    void driveclose(){
+        driver.close();
+    }
+
+}
